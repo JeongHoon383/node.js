7b28e9623d15ad7e5b5ff21a6f886a8015aca827
Jeong Hoon <gbg9609@naver.com>
2023-10-04 20:24:19 +0900

    10/4 node.js lecture second commit

--- router2/app.js
+++ router2/app.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import postsRouter from './route/posts.js'
+import userRouter from './route/user.js'
 
 const app = express();
 
 app.use('/posts', postsRouter)
+app.use('/user', userRouter)
 
 
 app.listen(8080)
--- router2/route/user.js
+++ router2/route/user.js
@@ -1,1 +1,20 @@
-//작업 해보기
+import express from 'express'
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  res.send('GET: /user')
+})
+
+router.post('/', (req, res, next) => {
+  res.send('POST: /user')
+});
+
+router.put('/:id', (req, res, next) => {
+  res.send(`PUT: /user/:id ==> ${req.params.id}`);
+})
+
+router.delete('/:id', (req, res, next) => {
+  res.send('DELETE: /user/:id');
+})
+
+export default router;
